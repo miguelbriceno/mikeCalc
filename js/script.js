52a79f366f7d84772149ece53bf395a854b71903
miguelbriceno <mabo544@gmail.com>
2022-04-04 15:47:44 -0500

    Optimizado y refactorizado

--- js/script.js
+++ js/script.js
@@ -9,338 +9,314 @@ var lastOp = "";
 // Eventlistener for number keys
 $(document).keydown(function(e) {
   var key = e.key;
+
   switch (key) {
     case "1":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "2":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "3":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "4":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "5":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "6":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "7":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "8":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "9":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case "0":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn(key);
       break;
 
     case ".":
-      numbersChain = numbersChain.concat(key);
-      $("#nums").text(numbersChain);
+      drawNumber(key);
+      animateBtn("dot");
       break;
 
     case "Delete":
-      numbersChain = "0";
-      acumulate = 0;
-      value = 0;
-      lastOp = "";
-      $("#nums").text(numbersChain);
-      $("#memory").text(numbersChain);
+      animateBtn("deleteAll");
+      eraseAll();
       break;
 
     case "+":
-      if (lastOp == "") {
-        lastOp = "suma";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "suma";
-        break;
-      }
+      animateBtn("plus");
+      callSuma();
+      break;
 
     case "-":
-      if (lastOp == "") {
-        lastOp = "resta";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "resta";
-        break;
-      }
+      animateBtn("minus");
+      callResta();
+      break;
 
     case "*":
-      if (lastOp == "") {
-        lastOp = "multiplicacion";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "multiplicacion";
-        break;
-      }
-
-      case "/":
-        if (lastOp == "") {
-          lastOp = "division";
-          acumulate = Number(numbersChain);
-          $("#memory").text(acumulate);
-          $("#nums").text("");
-          numbersChain = "";
-          break;
-        } else {
-          operation(lastOp, numbersChain);
-          numbersChain = "";
-          $("#nums").text(numbersChain);
-          lastOp = "division";
-          break;
-        }
-
-      case "Enter":
-        operation(lastOp, numbersChain);
-        numbersChain = "0";
-        lastOp = "";
-        value = 0;
-        $("#nums").text(numbersChain);
-        numbersChain = acumulate;
-        break;
-
-      case "Backspace":
-        numbersChain = numbersChain.slice(0, -1);
-        $("#nums").text(numbersChain);
-        break;
-
-      default:
-        break;
-    }
-});
-
-// Function that execute the math operations
-function operation(operation, numOnDisplay) {
-  switch (operation) {
-    case "suma":
-      value = Number(numOnDisplay);
-      acumulate = acumulate + value;
-      $("#memory").text(acumulate);
+      animateBtn("multiply");
+      callMultiply();
       break;
 
-    case "resta":
-      value = Number(numOnDisplay);
-      acumulate = acumulate - value;
-      $("#memory").text(acumulate);
+    case "/":
+      animateBtn("divide");
+      callDivide();
       break;
 
-    case "multiplicacion":
-      value = Number(numOnDisplay);
-      acumulate = acumulate * value;
-      $("#memory").text(acumulate);
+    case "Enter":
+      animateBtn("equal");
+      callEqual();
       break;
 
-    case "division":
-      value = Number(numOnDisplay);
-      acumulate = (acumulate / value);
-      $("#memory").text(acumulate);
+    case "Backspace":
+      animateBtn("backDelete");
+      callErase();
       break;
 
     default:
       break;
-  }
-}
+    }
+});
 
 //Add event listener to all buttons and animate them
 $("button").click(function() {
-  $(this).addClass("btnPressed");
-  setTimeout(function(){
-    $("button").removeClass("btnPressed");
-  },100);
   var btnPressed = $(this).attr("name");
+  var toAnimate = $(this).attr("id");
+  animateBtn(toAnimate);
 
   switch (btnPressed) {
     case "1":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "2":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "3":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "4":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "5":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "6":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "7":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "8":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "9":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
     case "0":
-      numbersChain = numbersChain.concat(btnPressed);
-      $("#nums").text(numbersChain);
+      drawNumber(btnPressed);
       break;
 
-    case "dot":
-      numbersChain = numbersChain.concat(".");
-      $("#nums").text(numbersChain);
+    case ".":
+      drawNumber(btnPressed);
       break;
 
     case "deleteAll":
-      numbersChain = "0";
-      acumulate = 0;
-      value = 0;
-      lastOp = "";
-      $("#nums").text(numbersChain);
-      $("#memory").text(numbersChain);
+      eraseAll();
       break;
 
     case "equal":
-      operation(lastOp, numbersChain);
-      numbersChain = "0";
-      lastOp = "";
-      value = 0;
-      $("#nums").text(numbersChain);
-      numbersChain = acumulate;
+      callEqual();
       break;
 
     case "backDelete":
-      numbersChain = numbersChain.slice(0, -1);
-      $("#nums").text(numbersChain);
+      callErase();
       break;
 
     case "plus":
-      if (lastOp == "") {
-        lastOp = "suma";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "suma";
-        break;
-      }
+      callSuma();
+      break;
 
     case "minus":
-      if (lastOp == "") {
-        lastOp = "resta";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "resta";
-        break;
-      }
+      callResta();
+      break;
 
     case "multiply":
-      if (lastOp == "") {
-        lastOp = "multiplicacion";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "multiplicacion";
-        break;
-      }
+      callMultiply();
+      break;
 
     case "divide":
-      if (lastOp == "") {
-        lastOp = "division";
-        acumulate = Number(numbersChain);
-        $("#memory").text(acumulate);
-        $("#nums").text("");
-        numbersChain = "";
-        break;
-      } else {
-        operation(lastOp, numbersChain);
-        numbersChain = "";
-        $("#nums").text(numbersChain);
-        lastOp = "division";
-        break;
-      }
+      callDivide();
+      break;
 
     default:
       break;
   }
 })
+
+// Function to draw numbers on screen
+function drawNumber(number){
+  numbersChain = numbersChain.concat(number);
+  $("#nums").text(numbersChain);
+}
+
+// Function to animate buttons
+function animateBtn(btn){
+  btn = "#" + btn;
+  $(btn).addClass("btnPressed");
+  setTimeout(function(){
+    $(btn).removeClass("btnPressed");
+  },100);
+}
+
+// Functions to erase
+function callErase(){
+  numbersChain = numbersChain.slice(0, -1);
+  $("#nums").text(numbersChain);
+}
+
+function eraseAll(){
+  numbersChain = "0";
+  acumulate = 0;
+  value = 0;
+  lastOp = "";
+  $("#nums").text(numbersChain);
+  $("#memory").text(numbersChain);
+}
+
+// Function that execute the math operations
+function callEqual(){
+  operation(lastOp, numbersChain);
+  numbersChain = "0";
+  lastOp = "";
+  value = 0;
+  $("#nums").text(numbersChain);
+  numbersChain = acumulate;
+}
+
+function callSuma(){
+  if (lastOp == "") {
+    lastOp = "suma";
+    acumulate = Number(numbersChain);
+    $("#memory").text(acumulate);
+    $("#nums").text("");
+    numbersChain = "";
+  } else {
+    operation(lastOp, numbersChain);
+    numbersChain = "";
+    $("#nums").text(numbersChain);
+    lastOp = "suma";
+  }
+}
+
+function callResta() {
+  if (lastOp == "") {
+    lastOp = "resta";
+    acumulate = Number(numbersChain);
+    $("#memory").text(acumulate);
+    $("#nums").text("");
+    numbersChain = "";
+  } else {
+    operation(lastOp, numbersChain);
+    numbersChain = "";
+    $("#nums").text(numbersChain);
+    lastOp = "resta";
+  }
+}
+
+function callMultiply() {
+  if (lastOp == "") {
+    lastOp = "multiplicacion";
+    acumulate = Number(numbersChain);
+    $("#memory").text(acumulate);
+    $("#nums").text("");
+    numbersChain = "";
+  } else {
+    operation(lastOp, numbersChain);
+    numbersChain = "";
+    $("#nums").text(numbersChain);
+    lastOp = "multiplicacion";
+  }
+}
+
+function callDivide(){
+  if (lastOp == "") {
+    lastOp = "division";
+    acumulate = Number(numbersChain);
+    $("#memory").text(acumulate);
+    $("#nums").text("");
+    numbersChain = "";
+  } else {
+    operation(lastOp, numbersChain);
+    numbersChain = "";
+    $("#nums").text(numbersChain);
+    lastOp = "division";
+  }
+}
+
+function operation(operation, numOnDisplay) {
+  switch (operation) {
+    case "suma":
+    value = Number(numOnDisplay);
+    acumulate = acumulate + value;
+    $("#memory").text(acumulate);
+    break;
+
+    case "resta":
+    value = Number(numOnDisplay);
+    acumulate = acumulate - value;
+    $("#memory").text(acumulate);
+    break;
+
+    case "multiplicacion":
+    value = Number(numOnDisplay);
+    acumulate = acumulate * value;
+    $("#memory").text(acumulate);
+    break;
+
+    case "division":
+    value = Number(numOnDisplay);
+    acumulate = (acumulate / value);
+    $("#memory").text(acumulate);
+    break;
+
+    default:
+    break;
+  }
+}
