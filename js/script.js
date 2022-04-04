2f7f3a5b0930ef901fc22d1d05116ab70e254c14
miguelbriceno <mabo544@gmail.com>
2022-04-04 14:28:45 -0500

    Falta optimizar codigo

--- js/script.js
+++ js/script.js
@@ -197,4 +197,150 @@ $("button").click(function() {
   },100);
   var btnPressed = $(this).attr("name");
 
+  switch (btnPressed) {
+    case "1":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "2":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "3":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "4":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "5":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "6":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "7":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "8":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "9":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "0":
+      numbersChain = numbersChain.concat(btnPressed);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "dot":
+      numbersChain = numbersChain.concat(".");
+      $("#nums").text(numbersChain);
+      break;
+
+    case "deleteAll":
+      numbersChain = "0";
+      acumulate = 0;
+      value = 0;
+      lastOp = "";
+      $("#nums").text(numbersChain);
+      $("#memory").text(numbersChain);
+      break;
+
+    case "equal":
+      operation(lastOp, numbersChain);
+      numbersChain = "0";
+      lastOp = "";
+      value = 0;
+      $("#nums").text(numbersChain);
+      numbersChain = acumulate;
+      break;
+
+    case "backDelete":
+      numbersChain = numbersChain.slice(0, -1);
+      $("#nums").text(numbersChain);
+      break;
+
+    case "plus":
+      if (lastOp == "") {
+        lastOp = "suma";
+        acumulate = Number(numbersChain);
+        $("#memory").text(acumulate);
+        $("#nums").text("");
+        numbersChain = "";
+        break;
+      } else {
+        operation(lastOp, numbersChain);
+        numbersChain = "";
+        $("#nums").text(numbersChain);
+        lastOp = "suma";
+        break;
+      }
+
+    case "minus":
+      if (lastOp == "") {
+        lastOp = "resta";
+        acumulate = Number(numbersChain);
+        $("#memory").text(acumulate);
+        $("#nums").text("");
+        numbersChain = "";
+        break;
+      } else {
+        operation(lastOp, numbersChain);
+        numbersChain = "";
+        $("#nums").text(numbersChain);
+        lastOp = "resta";
+        break;
+      }
+
+    case "multiply":
+      if (lastOp == "") {
+        lastOp = "multiplicacion";
+        acumulate = Number(numbersChain);
+        $("#memory").text(acumulate);
+        $("#nums").text("");
+        numbersChain = "";
+        break;
+      } else {
+        operation(lastOp, numbersChain);
+        numbersChain = "";
+        $("#nums").text(numbersChain);
+        lastOp = "multiplicacion";
+        break;
+      }
+
+    case "divide":
+      if (lastOp == "") {
+        lastOp = "division";
+        acumulate = Number(numbersChain);
+        $("#memory").text(acumulate);
+        $("#nums").text("");
+        numbersChain = "";
+        break;
+      } else {
+        operation(lastOp, numbersChain);
+        numbersChain = "";
+        $("#nums").text(numbersChain);
+        lastOp = "division";
+        break;
+      }
+
+    default:
+      break;
+  }
 })
